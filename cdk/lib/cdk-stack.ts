764f8a03cfc4ff3ad1f656ef25a1a7cfd4d31d9c
tadakun8 <tadakun8.lab@gmail.com>
2022-02-08 16:00:36 +0900

    feat: Create server access log bucket

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,15 +1,37 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import { CONSTANTS } from "./constants";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create Server Access Log Bucket
+    new s3.Bucket(this, CONSTANTS.serverAccessLogBucketName, {
+      bucketName: CONSTANTS.serverAccessLogBucketName,
+      // NOTE: Server access log bucket does not support KMS encryption, so S3_MANAGED(Server-side encryption) must be specified.
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      lifecycleRules: [
+        {
+          abortIncompleteMultipartUploadAfter: cdk.Duration.days(60),
+          expiration: cdk.Duration.days(365),
+          transitions: [
+            {
+              storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+              transitionAfter: cdk.Duration.days(60),
+            },
+            {
+              storageClass: s3.StorageClass.GLACIER,
+              transitionAfter: cdk.Duration.days(90),
+            },
+            {
+              storageClass: s3.StorageClass.DEEP_ARCHIVE,
+              transitionAfter: cdk.Duration.days(180),
+            },
+          ],
+        },
+      ],
+    });
   }
 }
---
+++ cdk/lib/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Define constants. Change values as needed for your project.
+ */
+export const CONSTANTS = {
+  /**
+   * Define server access log bucket name
+   */
+  serverAccessLogBucketName: "cdk-sample-server-access-log-bucket-tadakun8",
+};
