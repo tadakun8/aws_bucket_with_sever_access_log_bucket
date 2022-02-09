f4fa24142040455611984c03f701609909f6bd5d
tadakun8 <tadakun8.lab@gmail.com>
2022-02-09 21:18:36 +0900

    chore: Add comment

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -11,11 +11,17 @@ export class CdkStack extends cdk.Stack {
       bucketName: CONSTANTS.serverAccessLogBucketName,
       // NOTE: Server access log bucket does not support KMS encryption, so S3_MANAGED(Server-side encryption) must be specified.
       encryption: s3.BucketEncryption.S3_MANAGED,
+      // Server access log bucket does not need to be disclosed externally.
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      // This lifecycle rule is the following.
+      // 1. Garbage files from incomplete multipart uploads will be deleted after 60 days.
+      // 2. After 60 days, transit from standard class to IA class.
+      // 3. After 90 days, transit from IA class to Glacier Flexible Retrieval.
+      // 4. After 180 days, transit from glacier to deep achive glacier.
+      // 5. After 360 days, the object will be deleted.
       lifecycleRules: [
         {
           abortIncompleteMultipartUploadAfter: cdk.Duration.days(60),
-          expiration: cdk.Duration.days(365),
           transitions: [
             {
               storageClass: s3.StorageClass.INFREQUENT_ACCESS,
@@ -30,6 +36,7 @@ export class CdkStack extends cdk.Stack {
               transitionAfter: cdk.Duration.days(180),
             },
           ],
+          expiration: cdk.Duration.days(365),
         },
       ],
     });
